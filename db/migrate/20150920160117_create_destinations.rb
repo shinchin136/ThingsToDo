7eba9823a7a433fd1df049175c3a5f8b5995b28a
Shin Chin <shinchin136@gmail.com>
2015-09-20 12:01:35 -0400

    generated destination model

---
+++ db/migrate/20150920160117_create_destinations.rb
@@ -0,0 +1,10 @@
+class CreateDestinations < ActiveRecord::Migration
+  def change
+    create_table :destinations do |t|
+      t.string :city
+      t.string :country
+
+      t.timestamps null: false
+    end
+  end
+end
